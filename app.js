5879f9a01b32b31ceb2089f427d7d94422323439
tvrsimhan <raghavtv27@gmail.com>
2023-04-13 11:44:53 +0530

    Sends index page on root endpoint

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-import express from "express"
-import { cors } from "./middleware.js"
+const express = require("express")
+const fs = require("fs/promises")
+const path = require("path")
+
+let songs = []
+async function loadSongs() {
+    let files = await fs.readdir(path.join("songs"))
+    files.forEach((file) => {
+        if (file.endsWith(".txt")) {
+            songs.push(file)
+        }
+    })
+}
+loadSongs()
+
 
 const app = express()
 const PORT = process.env.PORT || 3003
@@ -11,6 +24,11 @@ app.listen(PORT, () => {
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/", cors, (_, res) => {
-	res.status(200).send("stuff")
+app.get("/", async (req, res) => {
+    let indexHTML = await fs.readFile("index.html", "utf8")
+
+    res.status(200).send(
+        indexHTML
+    )
+})
 })
